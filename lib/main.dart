368a1ab0531847ea1ade0ff8b1a7d933fdddf81c
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2020-01-29 12:37:51 +0100

    completed dice challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
+      home: SafeArea(
+        child: Scaffold(
           backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
         ),
-        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    void updateDiceValue() {
+      setState(() {
+        rightDiceNumber = Random().nextInt(6) + 1;
+        leftDiceNumber = Random().nextInt(6) + 1;
+      });
+    }
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () => updateDiceValue(),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () => updateDiceValue(),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
